6e51cff5077fc00e0e520a651c7fab7ff2798eec
nding1718 <nding1718@gmail.com>
2018-10-24 22:22:28 -0700

    add an new information analysis funcition use Mysql as backend database

---
+++ runninginformation-analysis-service/src/main/java/com/nding/backend/runninginformationanalysisservice/RunninginformationAnalysisServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nding.backend.runninginformationanalysisservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunninginformationAnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunninginformationAnalysisServiceApplication.class, args);
+	}
+}
---
+++ runninginformation-analysis-service/src/test/java/com/nding/backend/runninginformationanalysisservice/RunninginformationAnalysisServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nding.backend.runninginformationanalysisservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RunninginformationAnalysisServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
