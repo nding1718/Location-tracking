bca7a3ecd8b5cc926e1e60997870587ef57e3659
nding1718 <nding1718@gmail.com>
2018-11-03 15:30:17 -0700

    build first basic service

--- runninginformation-analysis-service/src/main/java/com/nding/backend/runninginformationanalysisservice/service/RunningInformationService.java
+++ runninginformation-analysis-service/src/main/java/com/nding/backend/runninginformationanalysisservice/service/RunningInformationService.java
@@ -1,4 +1,15 @@
 package com.nding.backend.runninginformationanalysisservice.service;
 
+import com.nding.backend.runninginformationanalysisservice.domain.RunningInformation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface RunningInformationService {
+    public List<RunningInformation> saveRunningInformation(List<RunningInformation> runningInformationList);
+    public Page<RunningInformation> findByHeartRate(int heartRate, Pageable pageable);
+    public Page<RunningInformation> findByHeartRateGreaterThan(int heartRate, Pageable pageable);
+    public Page<RunningInformation> findAllRunningInformationOrderByHealthLevel(Pageable pageable);
+    public void deleteAll();
 }
--- runninginformation-analysis-service/src/main/java/com/nding/backend/runninginformationanalysisservice/service/impl/RunningInformationServiceImpl.java
+++ runninginformation-analysis-service/src/main/java/com/nding/backend/runninginformationanalysisservice/service/impl/RunningInformationServiceImpl.java
@@ -1,4 +1,46 @@
 package com.nding.backend.runninginformationanalysisservice.service.impl;
 
-public class RunningInformationServiceImpl {
+import com.nding.backend.runninginformationanalysisservice.domain.RunningInformation;
+import com.nding.backend.runninginformationanalysisservice.domain.RunningInformationRepository;
+import com.nding.backend.runninginformationanalysisservice.service.RunningInformationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class RunningInformationServiceImpl implements RunningInformationService {
+
+    private RunningInformationRepository runningInformationRepository;
+
+    // constructor dependency injection
+    @Autowired
+    public RunningInformationServiceImpl(RunningInformationRepository runningInformationRepository) {
+        this.runningInformationRepository = runningInformationRepository;
+    }
+
+    @Override
+    public List<RunningInformation> saveRunningInformation(List<RunningInformation> runningInformationList) {
+        return null;
+    }
+
+    @Override
+    public Page<RunningInformation> findByHeartRate(int heartRate, Pageable pageable) {
+        return runningInformationRepository.findByHeartRate(heartRate, pageable);
+    }
+
+    @Override
+    public Page<RunningInformation> findByHeartRateGreaterThan(int heartRate, Pageable pageable) {
+        return runningInformationRepository.findByHeartRateGreaterThan(heartRate, pageable);
+    }
+
+    @Override
+    public Page<RunningInformation> findAllRunningInformationOrderByHealthLevel(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
