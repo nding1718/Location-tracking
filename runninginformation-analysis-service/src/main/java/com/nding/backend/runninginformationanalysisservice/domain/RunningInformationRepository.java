c2c35c66a968b5b233211d44a7a108ba3c9d2f2a
nding1718 <nding1718@gmail.com>
2018-11-03 15:10:26 -0700

    add DAO class

---
+++ runninginformation-analysis-service/src/main/java/com/nding/backend/runninginformationanalysisservice/domain/RunningInformationRepository.java
@@ -0,0 +1,13 @@
+package com.nding.backend.runninginformationanalysisservice.domain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface RunningInformationRepository extends JpaRepository<RunningInformation, Long> {
+    Page<RunningInformation>  findByHeartRateGreaterThan(@Param("heartRate") int heartRate, Pageable pageable);
+    Page<RunningInformation>  findByHeartRate(@Param("heartRate") int heartRate, Pageable pageable);
+}
+
+
